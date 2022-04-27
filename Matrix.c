dc4a068789ba0e3427fdf917d219332afe1b82c4
Kevin Raj <kev.raj@yahoo.com>
2022-04-27 09:28:02 -0400

    Edits to Matrx.c

--- Matrix.c
+++ Matrix.c
@@ -9,29 +9,19 @@
 #include<stdlib.h>
 
 void inputCheck(int input1, int input2){
-    //if(input1 == \0) {printf("Input 1 is null. Please input valid command line arguements.");}
-        
 
-    //if(input1 == \0) {printf("Input 2 is null. Please input valid command line arguements.");}
-        
-    printf("I am here\n %d %d\n", input1, input2);
+    if(input1 <= 0) {printf("Input 1 is %d. Please input non-zero positive command line arguements.\n", input1);}
+    if(input2 <= 0) {printf("Input 2 is %d. Please input non-zero positive command line arguements.\n", input2);}
 
-    if(input1 < 0) {printf("Input 1 is negative. Please input positive command line arguements.");}
-
-    if(input2 < 0) {printf("Input 2 is negative. Please input positive command line arguements.");}
 }
 
 int main(int argc, char *argv[]) {
-    
 
     if(argc != 3){
         printf("Please use this command format: ./Matrix.c <rows> <columns>\n");
     }
 
-    inputCheck(argv[1], argv[2]);
-    
-
-
+    inputCheck(atoi(argv[1]), atoi(argv[2]));
 
 }
 
