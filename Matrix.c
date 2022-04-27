63463dbca84c383609bbda14b9ef17afbf78a899
Kevin Raj <kev.raj@yahoo.com>
2022-04-27 11:08:50 -0400

    Edits to Matrix.c

--- Matrix.c
+++ Matrix.c
@@ -7,11 +7,41 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
-void inputCheck(int input1, int input2){
+bool inputCheck(int input1, int input2){
 
-    if(input1 <= 0) {printf("Input 1 is %d. Please input non-zero positive command line arguements.\n", input1);}
-    if(input2 <= 0) {printf("Input 2 is %d. Please input non-zero positive command line arguements.\n", input2);}
+    if(input1 <= 0) {
+        printf("Input 1 is %d. Please input non-zero positive command line arguements.\n", input1);
+        return false;    
+    }
+    if(input2 <= 0) {
+        printf("Input 2 is %d. Please input non-zero positive command line arguements.\n", input2);
+        return false;    
+    }
+    return true;
+
+}
+
+
+void mainMenu(){
+    const width = 62;
+
+    char c[] = "Hey. Welcome to the Matrix calculator. This calculator will\n"
+               "perform calcuations that deal with Matrix manipulation.\n"
+               "This calculator is built on C and was built by Kevin Raj\n";
+               
+    for(int i = 0; i < width; i++)
+        printf("*");
+    
+    for(int i = 0; i <= width; i++) {
+        if(i == 0) {
+            printf("*  ");
+        } else if (i == width) {
+            /* code */
+        }
+        
+    }
 
 }
 
@@ -21,7 +51,12 @@ int main(int argc, char *argv[]) {
         printf("Please use this command format: ./Matrix.c <rows> <columns>\n");
     }
 
-    inputCheck(atoi(argv[1]), atoi(argv[2]));
+    int row = atoi(argv[1]);
+    int col = atoi(argv[2]);
+
+    if(!inputCheck(row, col)) { exit(1); }
+    
+    int matrix[row][col];
 
 }
 
