d0d96624aa71e2495ecb0345584385514b58aefb
Kevin Raj <kev.raj@yahoo.com>
2022-05-02 08:57:05 -0400

    Edited Matrix.c

--- Matrix.c
+++ Matrix.c
@@ -9,11 +9,100 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
+
+int operation;
+
+typedef struct{
+    int row;
+    int col;
+} Matrix;
+
+void mainMenu();
+bool inputCheck(int input1, int input2);
+Matrix newMatrix(Matrix matrix, int row, int col);
+void scale(Matrix matrix, int factor);
+
+
+
+
+int main(int argc, char *argv[]) {
+
+    if(argc != 3){
+        printf("Please use this command format: ./Matrix.c <rows> <columns>\n");
+    }
+
+    int row = atoi(argv[1]);
+    int col = atoi(argv[2]);
+
+    if(!inputCheck(row, col)) { exit(1); }
+
+    mainMenu();
+
+    
+
+    switch(operation){
+        case 1:
+            // Scaling method call
+            Matrix matrix = newMatrix(matrix, row, col);
+            populateMatrix(matrix);
+
+            break;
+        case 2:
+            // Adding method call and get second matrix
+            break;
+        case 3:
+            // Subtract method call and get second matrix
+            break;
+        case 4:
+            // Multiply method call and get second matrix
+            break;
+    }
+
+}
+
+
+
+
+
+Matrix newMatrix(Matrix matrix, int row, int col){
+    matrix.row = row;
+    matrix.col = col;
+
+    int arr[row][col];
+
+    matrix = arr;
+
+    return matrix;
+}
+
+
+Matrix populateMatrix(Matrix matrix){
+
+    printf("  ");
+    for(int i = 1; i <= 3; i++){
+        printf("%d  ", i);
+    }
+
+}
+
+
+
+void scale(Matrix matrix, int factor){
+    for(int i = 0; i < matrix.row; i++){
+        for(int j = 0; j < matrix.col; j++){
+            matrix.array[i][j] = factor * matrix.array[i][j];
+        }
+    }
+}
+
+
+
+
 bool inputCheck(int input1, int input2){
 
     if(input1 <= 0) {
         printf("Input 1 is %d. Please input non-zero positive command line arguements.\n", input1);
-        return false;    
+        return false;
     }
     if(input2 <= 0) {
         printf("Input 2 is %d. Please input non-zero positive command line arguements.\n", input2);
@@ -22,6 +111,8 @@ bool inputCheck(int input1, int input2){
     return true;
 
 }
+
+
 
 
 void mainMenu(){
@@ -67,29 +158,11 @@ void mainMenu(){
 
     printf("\n\n");
 
-    int selection;
-    printf("$ Input Selection(1-4): ");
-    scanf("%d", &selection);
-
     
-
-}
-
-
-int main(int argc, char *argv[]) {
-
-    if(argc != 3){
-        printf("Please use this command format: ./Matrix.c <rows> <columns>\n");
-    }
-
-    int row = atoi(argv[1]);
-    int col = atoi(argv[2]);
-
-    if(!inputCheck(row, col)) { exit(1); }
-
-    mainMenu();
-
-    ///int Matrix[row][col];
-
+    do {
+        printf("$ Input Selection(1-4): ");
+        scanf("%d", &operation);
+    } while(!(operation >= 1 && operation <= 4));
+    
 }
 
